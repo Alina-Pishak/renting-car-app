6b0baa8f8f97151def444581354c16884380c93d
Alina-Pishak <alinapishak@icloud.com>
2024-02-13 16:38:13 +0200

    add lazy import and suspense

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,38 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "../Layout/Layout";
-import HomePage from "../pages/HomePage/HomePage";
-import CatalogPage from "../pages/CatalogPage/CatalogPage";
-import FavoritesPage from "../pages/FavoritesPage/FavoritesPage";
+import { Suspense, lazy } from "react";
+import { Box, CircularProgress } from "@mui/material";
+
+const HomePage = lazy(() => import("../pages/HomePage/HomePage"));
+const CatalogPage = lazy(() => import("../pages/CatalogPage/CatalogPage"));
+const FavoritesPage = lazy(() =>
+  import("../pages/FavoritesPage/FavoritesPage")
+);
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage />} />
-        <Route path="catalog" element={<CatalogPage />} />
-        <Route path="favorites" element={<FavoritesPage />} />
-        <Route path="*" element={<HomePage />} />
-      </Route>
-    </Routes>
+    <Suspense
+      fallback={
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "50%",
+          }}
+        >
+          <CircularProgress />
+        </Box>
+      }
+    >
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="catalog" element={<CatalogPage />} />
+          <Route path="favorites" element={<FavoritesPage />} />
+          <Route path="*" element={<HomePage />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
 
